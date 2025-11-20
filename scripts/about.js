02488a1564e5f908b8794f9de87e6f857a6c78a4
gasperlevpuscek <gasp.levp69@gmail.com>
2025-11-20 19:29:29 +0100

    Added an about

---
+++ scripts/about.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const logo = document.getElementById("deviant_logo");
+
+    logo.addEventListener("click", () => {
+        Swal.fire({
+            html: `
+                <h1>DeviantArt Form Copy</h1>
+                <b>Author:</b> Gašper Levpušček <br>
+                <b>Class:</b> 4. Rb <br>
+                <b>Project:</b> DeviantArt Form Copy`,
+            imageUrl: "/images/deviantart-icon.svg",
+            imageWidth: 80,     // optional
+            imageHeight: 80,    // optional
+            imageAlt: "Project Icon"
+        });
+    });
+
+});
